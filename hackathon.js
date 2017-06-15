7b0595ec1862d625381ec2afe3070ce42f903902
Gal Niv <gal.niv@gmail.com>
2017-06-14 22:39:31 -0400

    Add Thomas's changes

--- hackathon.js
+++ hackathon.js
@@ -6,6 +6,8 @@ window.Stepper = function() {
     waiting: false
   };
 
+
+
   var addStep = function(title, stepFunction) {
     state.sidebar.addItem(title);
     steps.push({ title: title, stepFunction: stepFunction });
@@ -33,7 +35,7 @@ window.Stepper = function() {
   var setSidebar = function(sidebar) {
     state.sidebar = sidebar;
   }
-  
+
   return {
     addStep: addStep,
     step: nextStep,
@@ -63,14 +65,9 @@ window.onload = function() {
   var raisedShadowFilter = Snap('#drop-shadow-raise');
   var checkIcon = Snap('#check-icon');
   var closeIcon = Snap('#close-icon');
-  var fileIcon = Snap('#file-icon');
   var securityGateLeft = paper.image('security-gate-left.png', 630, 60, 100, 100);
   var securityGateRight = paper.image('security-gate-right.png', 279, 60, 100, 100);
   var securityGate = [ securityGateLeft, securityGateRight ];
-  var databaseIcon = Snap('#database').attr({ height: 150, width: 150, x: -150, y: 300 });
-  paper.append(databaseIcon);
-  paper.append(fileIcon);
-  fileIcon.attr({ x: 260, y: 130, opacity: 0 });
 
   // BL box & gears
   var blBox = [];
@@ -78,7 +75,7 @@ window.onload = function() {
   blBox.push(paper.rect(blStartX, blStartY, 200, 100).attr({ opacity: 1, fill: '#a26102' }));
   blBox.push(paper.rect(blStartX, blStartY + 100, 200, 60).attr({ opacity: 1, fill: '#d78001' }).transform('m1,0,0.364,1,0,0t-182'));
   blBox.push(paper.rect(blStartX, blStartY, 200, 60).attr({ opacity: 1, fill: '#fb8c00' }).transform('m1,0,0.364,1,0,0t-145'));
-  
+
   var gear = Snap('#gear').transform('t' + (blStartX + 73) + ',' + (blStartY + 98) + 's2').attr({ fill: '#01579b' });
   var gear2 = gear.clone().transform('t' + (blStartX + 100) + ',' + (blStartY + 65) + 's1.7r18').attr({ fill: '#5d4037' });
   var gear3 = gear.clone().transform('t' + (blStartX + 133) + ',' + (blStartY + 77) + 's2.2r30').attr({ fill: '#004d40' });
@@ -105,6 +102,7 @@ window.onload = function() {
   mask.append(paper.circle(blStartX + 120, blStartY + 110, 50).attr({ opacity: 0.5, fill: 'black' }));
   frontWall.attr({ mask: mask })
 
+  // setAll(blBox, { opacity: 0 });
   blGroup = paper.group();
   for (var i=0; i < blBox.length; i++) {
     blGroup.add(blBox[i]);
@@ -150,8 +148,8 @@ window.onload = function() {
   requestGroup = paper.group().attr({ filter: shadowFilter });
   var requestRect = paper.rect(200, -100, 150, 50).attr({ fill: '#2196f3' });
   requestGroup.add(requestRect);
-  blFrontGroup.before(requestGroup);
-  
+  blFrontGroup.before(requestRect);
+
   // Security gate
   var securityGateLights = paper.group();
   var lightLeft = paper.group().append(Snap('#security-gate-light'));
@@ -185,7 +183,6 @@ window.onload = function() {
     dotCycleState = (dotCycleState + 1) % 4;
     setTimeout(animateDots, 500);
   }
-
   animateDots();
 
   securityGateLights.blink = function() {
@@ -218,7 +215,6 @@ window.onload = function() {
   var line = paper.line(0, 152, 610, 152).attr({ stroke: '#f1582c', strokeWidth: 2, strokeDasharray: '10 10 10', strokeDashoffset: 0 });
   conveyorBelt.push(line);
   conveyorBelt.push(paper.line(0, 155, 610, 155).attr({ stroke: '#f1582c', strokeWidth: 5 }));
-
   var lastOffsetValue = 0;
   animateBelt = function() {
     Snap.animate(lastOffsetValue, lastOffsetValue-50, function(offsetValue){
@@ -226,93 +222,92 @@ window.onload = function() {
       lastOffsetValue = offsetValue % 400;
     }, 1000, mina.linear, animateBelt);
   };
-
   securityGateGroup.after(conveyorBelt[1]);
   blFrontGroup.before(conveyorBelt[0]);
   blFrontGroup.before(conveyorBelt[1]);
 
-  stepper.addStep('Request comes in', function(done) {
-    requestRect.animate({ transform: 't0,200' }, 1000, mina.bounce, function() {
-      done(true);
-    });
-  });
+  stepper.loadSteps = function() {
+    var textarea = document.getElementById('steps');
+    var steps = textarea.value;
 
-  // Generate request ID
-  stepper.addStep('Generate request ID', function(done) {
-    var requestId = {}
-    requestId.rect = paper.rect(25, 100, 25, 50).attr({ fill: '#ff5252', filter: shadowFilter });
-    requestId.idText = paper.text(30, 130, 'ID');
-    requestId.group = paper.group(requestId.rect, requestId.idText).attr({ opacity: 0 });
-    requestId.group.animate({ opacity: 1 }, 750, mina.easeinout, function() {
-      requestId.group.animate({ transform: 't150' }, 400, mina.easein, function() {
-        requestId.rect.attr({ filter: '' });
-        requestGroup.add(requestId.group);
-        requestId.rect.animate({ fill: '#42a5f5' }, 250, function() {
-          done(true);
-        });
+    try {
+      steps = JSON.parse(steps);
+    } catch (e) {}
+
+    stepper.addStep('New Request', function(done) {
+      requestRect.animate({ transform: 't0,200' }, 1000, mina.bounce, function() {
+        done(true);
       });
     });
-  });
 
-  // Parse request body
-  stepper.addStep('Parse request body', function(done) {
-    var jsonText = paper.text(257, 130, '{ ... }');
-    requestGroup.add(jsonText);
-    var textCover = paper.rect(200, 115, 150, 30).attr({ fill: '#2196f3' });
-    var scanLine = paper.line(380, 80, 380, 170).attr({ stroke: '#000000', strokeWidth: 3, opacity: 0 });
-    scanLine.animate({ opacity: 1 }, 750, mina.easeinout, function() {
-      scanLine.animate({ x1: 200, x2: 200 }, 2000, mina.easeinout, function() {
-        textCover.animate({ transform: textCover.transform().localMatrix.scale(0, 1, 350, 115) }, 1800, mina.easeinout);
-        scanLine.animate({ x1: 380, x2: 380 }, 2000, mina.easeinout, function() {
-          scanLine.animate({ opacity: 0 }, 250, mina.easeinout, function() {
-            done(true);
-          });
-        });
-      });
-    });
-  });
-  
-  // Validate credentials
-  stepper.addStep('Validate credentials', function(done) {
-    securityGateLights.blink();
-    animateBelt();
-    securityGateGroup.animate({ transform: 't-400' }, 10000, mina.linear)
-    leftGroup.animate({ transform: 't-750' }, 10000, mina.linear)
-    setTimeout(function() {
-      securityGateLights.stopBlink(false);
-      done(true);
-    }, 6000);
-  });
+    steps.forEach(function(step) {
+      if (step.key === 'request_id') {
+        stepper.addStep(step.title, function(done) {
+          document.getElementById('step-metadata').innerHTML = 'Request id: ' + step.id;
 
-  // Pre-hook BL
-  stepper.addStep('Business Logic pre-hook', function(done) {
-    blGroup.animate({ transform: 't-400,-370' }, 10000, mina.linear);
-    blFrontGroup.animate({ transform: 't-400,-370' }, 10000, mina.linear);
-
-    setTimeout(function() {
-      done(true);
-    }, 6000);
-  });
-
-  stepper.addStep('Save data', function(done) {
-    databaseIcon.animate({ x: 200 }, 500, mina.easeout, function() {
-      var databaseIconBounceDelta = 5;
-      databaseIconBounce = function() {
-        databaseIcon.animate({ y: 300 + databaseIconBounceDelta }, 1000, mina.easeinout, databaseIconBounce);
-        databaseIconBounceDelta = databaseIconBounceDelta * -1;
-      }
-      databaseIconBounce();
-      fileIcon.animate({ opacity: 0.7 }, 1000, mina.easein, function() {
-        fileIcon.animate({ transform: 't0,230' }, 3000, mina.easeinout, function() {
-          fileIcon.animate({ opacity: 0 }, 2500, mina.easein, function() {
-            databaseIcon.animate({ x: 630 }, 500, mina.easeout, function() {
-              done(true);
+          var requestId = {}
+          requestId.rect = paper.rect(25, 100, 25, 50).attr({ fill: '#ff5252', filter: shadowFilter });
+          requestId.idText = paper.text(30, 130, 'Id');
+          requestId.group = paper.group(requestId.rect, requestId.idText).attr({ opacity: 0 });
+          requestId.group.animate({ opacity: 1 }, 750, mina.easeinout, function() {
+            requestId.group.animate({ transform: 't150' }, 400, mina.easein, function() {
+              requestId.rect.attr({ filter: '' });
+              requestGroup.add(requestId.group);
+              requestId.rect.animate({ fill: '#42a5f5' }, 250, function() {
+                done(true);
+              });
             });
           });
         });
-      });
+      } else if (step.key === 'authentication') {
+        stepper.addStep(step.title, function(done) {
+          if (step.error) {
+            document.getElementById('step-metadata').innerHTML = 'Authentication error: ' + step.error.kcs.debug;
+          } else {
+            document.getElementById('step-metadata').innerHTML = 'Authentication succeded';
+          }
+          securityGateLights.blink();
+          animateBelt();
+          securityGateGroup.animate({ transform: 't-400' }, 10000, mina.linear)
+          leftGroup.animate({ transform: 't-750' }, 10000, mina.linear)
+          setTimeout(function() {
+            securityGateLights.stopBlink(step.error ? true : false);
+            done(step.error ? false : true);
+          }, 6000);
+        });
+      }
     });
-  });
+  };
+
+  // // Parse request body
+  // stepper.addStep('Parse request body', function(done) {
+  //   var jsonText = paper.text(257, 130, '{ ... }');
+  //   requestGroup.add(jsonText);
+  //   var textCover = paper.rect(200, 115, 150, 30).attr({ fill: '#2196f3' });
+  //   var scanLine = paper.line(380, 80, 380, 170).attr({ stroke: '#000000', strokeWidth: 3, opacity: 0 });
+  //   scanLine.animate({ opacity: 1 }, 750, mina.easeinout, function() {
+  //     scanLine.animate({ x1: 200, x2: 200 }, 2000, mina.easeinout, function() {
+  //       textCover.animate({ transform: textCover.transform().localMatrix.scale(0, 1, 350, 115) }, 1800, mina.easeinout);
+  //       scanLine.animate({ x1: 380, x2: 380 }, 2000, mina.easeinout, function() {
+  //         scanLine.animate({ opacity: 0 }, 250, mina.easeinout, function() {
+  //           done(true);
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+
+  // // Pre-hook BL
+  // stepper.addStep('Business Logic pre-hook', function(done) {
+  //   blGroup.animate({ transform: 't-400,-370' }, 10000, mina.linear);
+  //   blFrontGroup.animate({ transform: 't-400,-370' }, 10000, mina.linear);
+
+  //   setTimeout(function() {
+  //     done(true);
+  //   }, 1000);
+  // });
+
+
 
   var canvasEdge = paper.rect(600, 25, 350, 600).attr({ fill: 'white' });
   canvasEdge.after(sidebarGroup);
